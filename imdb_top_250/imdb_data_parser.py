962b56586bc4b4d901d25106dbd68a98d1de7a8a
Dom Leone <ohheydom@gmail.com>
2016-06-13 19:38:45 -0400

    Add skeleton for analysis of wrestling deaths         Add csvs for mlb and nfl data         Minor fixes in imdb analysis

--- imdb_top_250/imdb_data_parser.py
+++ imdb_top_250/imdb_data_parser.py
@@ -29,7 +29,7 @@ class IMDBDataParser:
             content = soup.select('div#content-2-wide')[0]
 
             main_top = content.select('div#main_top div.title-overview div#title-overview-widget')[0]
-            d['rating'] = main_top.find('span', {'itemprop': 'ratingValue'}).text
+            d['rating'] = float(main_top.find('span', {'itemprop': 'ratingValue'}).text)
 
             title_bar = main_top.select('div.titleBar')[0]
             d['title'] = title_bar.h1.contents[0].strip().encode('utf-8')
@@ -46,11 +46,12 @@ class IMDBDataParser:
 
             credit_summary_items = plot_summary.find_all('div', class_='credit_summary_item')
             d['director'] = credit_summary_items[0].span.a.span.text.encode('utf-8').strip()
-            d['writer'] = credit_summary_items[1].span.a.span.text.encode('utf-8').strip()
+            writers = credit_summary_items[1].find_all('span', {'itemprop': 'name'})
+            d['writer'] = [w.text for w in writers]
 
             main_bottom = content.select('div#main_bottom')[0]
 
-            d['imdb_rating'] = str(re.search('\d+', main_bottom.find('a', {'href': '/chart/top?ref_=tt_awd'}).text).group(0))
+            d['imdb_rating'] = int(re.search('\d+', main_bottom.find('a', {'href': '/chart/top?ref_=tt_awd'}).text).group(0))
             cast = main_bottom.find('table', class_='cast_list').find_all('tr', class_=['even', 'odd'])
             for a in cast:
                 actor = {}
@@ -65,7 +66,6 @@ class IMDBDataParser:
             gross = title_details.find(self.div_contains_gross)
             d['box_office']['budget'] = money_converter(budget.contents[2].encode('utf-8').strip()) if budget else None
             d['box_office']['gross'] = money_converter(gross.contents[2].encode('utf-8').strip()) if gross else None
-            print d['box_office']['budget']
             return d
 
     def a_contains_country(self, elem):
@@ -91,5 +91,5 @@ class IMDBDataParser:
             movie_list.append(self.parse_file(f))
         return movie_list
 
-x = IMDBDataParser()
-x.parse_files('pages')
+#x = IMDBDataParser()
+#x.parse_files('pages')
