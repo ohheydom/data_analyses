ab014df8c6a82a5875e2e7cb5e451802ebff8582
Dom Leone <ohheydom@gmail.com>
2016-06-13 19:38:45 -0400

    Add skeleton for analysis of wrestling deaths         Add csvs for mlb and nfl data         Minor fixes in imdb analysis

--- imdb_top_250/helpers.py
+++ imdb_top_250/helpers.py
@@ -4,10 +4,9 @@ from decimal import Decimal
 #Exchange rates current as of June 8th, 2016
 xrates.install('money.exchange.SimpleBackend')
 xrates.base = 'USD'
-rates = {'UKP': 0.69, 'EUR': 0.88, 'DEM': 1.72, 'FRF': 5.77, 'INR': 66.50, 'AUD': 1.34, 'JPY': 107.07, 'RUR': 63.67}
+rates = {'EUR': 0.88}
 for k, v in rates.iteritems():
     xrates.setrate(k, Decimal(v))
-
 
 def money_converter(amount):
     """Returns value in USD
@@ -15,12 +14,7 @@ def money_converter(amount):
     if amount == None:
         return None
     if amount[0] == '$':
-        return amount[1:].replace(',', '')
-    if amount[0] == '\xc2': #Pound
-        return str(Money(amount[2:].replace(',', ''), 'UKP').to('USD')).replace(',', '')[4:]
+        return int(round(float(amount[1:].replace(',', ''))))
     if amount[0] == '\xe2': #Euro
-        return str(Money(amount[3:].replace(',', ''), 'EUR').to('USD')).replace(',', '')[4:]
-    pre = amount[0:3]
-    if pre in rates.keys():
-        return str(Money(amount[5:].replace(',', ''), pre).to('USD')).replace(',', '')[4:]
-    return amount
+        return int(round(float(str(Money(amount[3:].replace(',', ''), 'EUR').to('USD')).replace(',', '')[4:])))
+    return None
