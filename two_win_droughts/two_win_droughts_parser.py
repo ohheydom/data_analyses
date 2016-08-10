a86f9e3c570edfca6cd028184e95d250a7530c4f
Dom Leone <ohheydom@gmail.com>
2016-08-10 00:54:56 -0400

    Refactor two_wins method         Iterate over entire dataset only once         Extract duplicate code into separate methods

--- two_win_droughts/two_win_droughts_parser.py
+++ two_win_droughts/two_win_droughts_parser.py
@@ -31,6 +31,17 @@ def write_file(d, filename):
             new_e = e.strftime('%m/%d/%Y')
             f.write("{}|{}|{}|{}\n".format(t, vals['streak'], new_s, new_e))
 
+def update_dict(d, new_d, tn):
+    for (t, v) in new_d.iteritems():
+        if t in tn:
+            t = tn[t]
+        if not t in d:
+            d[t] = { k: v[k] for k in ['streak', 'start_date', 'end_date'] }
+        else:
+            if d[t]['streak'] < v['streak']:
+                d[t] = { k: v[k] for k in ['streak', 'start_date', 'end_date'] }
+    return d
+
 def alternate_wins(start_year):
     d = {}
     for f in range(start_year, 2016):
@@ -72,65 +83,40 @@ def alternate_wins(start_year):
 
                 temp_d[v]['past'] = temp_d[v]['current']
 
-        for (t, v) in temp_d.iteritems():
-            if t in tn:
-                t = tn[t]
-            if not t in d:
-                d[t] = { k: v[k] for k in ['streak', 'start_date', 'end_date'] }
-            else:
-                if d[t]['streak'] < v['streak']:
-                    d[t] = { k: v[k] for k in ['streak', 'start_date', 'end_date'] }
+        d = update_dict(d, temp_d, tn)
     return d
 
 def two_wins(start_year):
     d = {}
     for f in range(start_year, 2016):
         games = pd.read_csv('data/GL{}.TXT'.format(f), header=None)
-        teams = set(games[6])
-
-        for t in teams:
-            w_streak = 0
-            streak = 0
-            longest_streak = 0
-            start_date = None
-            end_date = None
-            gs = games[(games[3] == t) | (games[6] == t)].sort_values([0, 1])
-            for i, (_, g) in enumerate(gs.iterrows()):
-                if g[3] == t: #Away Team
-                    if g[9] > g[10]:
-                        w_streak += 1
-                        if w_streak >= 2:
-                            if streak > longest_streak:
-                                longest_streak = streak
-                                start_date = gs.iloc[i-streak][0]
-                                end_date = g[0]
-                            streak = 0
-                        else:
-                            streak += 1
-                    else:
-                        w_streak = 0
-                        streak += 1
-                else: #Home Team
-                    if g[10] > g[9]:
-                        w_streak += 1
-                        if w_streak >= 2:
-                            if streak > longest_streak:
-                                longest_streak = streak
-                                start_date = gs.iloc[i-streak][0]
-                                end_date = g[0]
-                            streak = 0
-                        else:
-                            streak += 1
-                    else:
-                        w_streak = 0
-                        streak += 1
-            if t in tn:
-                t = tn[t]
-            if not t in d:
-                d[t] = {'streak': longest_streak, 'start_date': start_date, 'end_date': end_date}
+        td = {}
+        gs = games.sort_values([0, 1])
+        for i, (_, g) in enumerate(gs.iterrows()):
+            h, a = g[6], g[3]
+            for v in [h, a]:
+                if not v in td:
+                    td[v] = { 'w_streak': 0, 'c_streak': 0, 'streak': 0, 't_start_date': g[0], 'start_date': None, 'end_date': None}
+            if g[9] > g[10]: 
+                w, l = a, h
             else:
-                if d[t]['streak'] < longest_streak:
-                    d[t] = {'streak': longest_streak, 'start_date': start_date, 'end_date': end_date}
+                w, l = h, a
+                
+            if td[l]['w_streak'] >=2:
+                td[l]['t_start_date'] = g[0]
+
+            td[w]['w_streak'] += 1
+            if td[w]['w_streak'] >= 2:
+                if td[w]['c_streak'] > td[w]['streak']:
+                    td[w]['streak'] = td[w]['c_streak']
+                    td[w]['end_date'] = g[0]
+                    td[w]['start_date'] = td[w]['t_start_date']
+                td[w]['c_streak'] = 0
+            else:
+                td[w]['c_streak'] += 1
+            td[l]['w_streak'] = 0
+            td[l]['c_streak'] += 1
+        d = update_dict(d, td, tn)
     return d
 
 def test_alternating_wins(d):
@@ -143,13 +129,13 @@ def test_two_wins(d):
     assert d['Chicago Cubs']['streak'] == 53, 'Incorrect value for the Cubs'
     assert d['Kansas City Royals']['streak'] == 38, 'Incorrect value for the Royals'
 
-start_year = 1900
-alt = alternate_wins(start_year)
-test_alternating_wins(alt)
-alt_file = 'txts/alternating-w-l'
-write_file(alt, alt_file)
+start_year = 2010
+#alt = alternate_wins(start_year)
+#test_alternating_wins(alt)
+#alt_file = 'txts/alternating-w-l'
+#write_file(alt, alt_file)
 
-#two = two_wins(start_year)
+two = two_wins(start_year)
 #test_two_wins(two)
 #two_file = 'txts/two-win-droughts'
 #write_file(two, two_file)
